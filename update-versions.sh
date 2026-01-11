f81bcc544801726a59f32008db1a288763bc9e7a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-11 01:47:59 +0100

    Initialize release

---
+++ update-versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -exuo pipefail
+
+version=$(curl -fsSL "https://api.github.com/repos/rakshasa/rtorrent/releases/latest" | jq -re .tag_name)
+flood_version=$(curl -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg flood_version "${flood_version//v/}" \
+    '.version = $version | .flood_version = $flood_version' <<< "${json}" | tee VERSION.json
