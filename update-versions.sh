41340e8054b3846151885d025089e86a4b7511fc
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-25 03:34:42 +0100

    add auth [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-url=$(curl -fsSL "https://api.github.com/repos/slskd/slskd/actions/runs?branch=master&exclude_pull_requests=true" | jq -re '[.workflow_runs[] | select(.name == "CI") | select(.conclusion == "success") | .artifacts_url][0]')
+url=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/slskd/slskd/actions/runs?branch=master&exclude_pull_requests=true" | jq -re '[.workflow_runs[] | select(.name == "CI") | select(.conclusion == "success") | .artifacts_url][0]')
 json_artifacts=$(jq -re '.artifacts[]' <<< "$(curl -fsSL "${url}?per_page=100")")
 version=$(jq -re '. | select(.name | contains("linux-musl-x64")) | .name' <<< "${json_artifacts}" | sed -e 's/slskd-//' -e 's/-.*//')
 version_url_amd64=$(jq -re '. | select(.name | contains("linux-musl-x64")) | .archive_download_url' <<< "${json_artifacts}")
